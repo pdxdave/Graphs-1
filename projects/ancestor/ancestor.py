dee59b75de29c852e5358700d8f2d460d2c4407c
pdxdave <webdevdave@hotmail.com>
2019-10-08 19:49:09 -0700

    working ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -22,18 +22,55 @@
 '''
 
 
-def earliest_ancestor(ancestors, starting_node):  
-    # create a vertices dictionary for the pairs
-    vertices = {}
-    # loop through the ancestors
-    for j in ancestors:
-        # we know that parent comes first in the pairs, so parent will be 0, child 1
-        parent = j[0]
-        child = j[1]
-        # if the child isn't in a vertices, make a set and add a parent
-        if child not in vertices:
-            vertices[child] = set()
-            vertices[child].add(parent)
-        # if it is, add a parent
-        elif vertices[child]:
-            vertices[child].add(parent)
+# def earliest_ancestor(ancestors, starting_node):  
+#     # create a vertices dictionary for the pairs
+#     vertices = {}
+#     # loop through the ancestors
+#     for j in ancestors:
+#         # we know that parent comes first in the pairs, so parent will be 0, child 1
+#         parent = j[0]
+#         child = j[1]
+#         # if the child isn't in a vertices, make a set and add a parent
+#         if child not in vertices:
+#             vertices[child] = set()
+#             vertices[child].add(parent)
+#         # if it is, add a parent
+#         elif vertices[child]:
+#             vertices[child].add(parent)
+from collections import defaultdict
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+def dfs(starting_vertex, family):
+    visited = []
+    ss = Stack()
+    ss.push([starting_vertex])
+    while ss.size() > 0:
+        path = ss.pop()
+        vertex = path[-1]
+        if vertex not in visited:
+            visited.append(vertex)
+        for neighbor in family[vertex]:
+            path_copy = [*path]
+            path_copy.append(neighbor)
+            ss.push(path_copy)
+    return visited[-1]
+def earliest_ancestor(ancestors, starting_node):
+    family = defaultdict(list)
+    for parent, child in ancestors:
+        family[child].append(parent)
+    if starting_node not in family:
+        return -1
+    ancestor = dfs(starting_node, family)
+    return ancestor
